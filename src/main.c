34eb5e3ba3c8aba35e9021b582f4d737973918bb
Maleehaakbar <maleehaashley@gmail.com>
2025-03-12 15:43:59 +0500

    set up connection callback

--- src/main.c
+++ src/main.c
@@ -7,8 +7,9 @@
 #include <dk_buttons_and_leds.h>       /*nrf/include*/
 #include <zephyr/bluetooth/uuid.h>
 #include <zephyr/bluetooth/addr.h>
+#include <zephyr/bluetooth/conn.h>
 
-LOG_MODULE_REGISTER(Lesson2_Exercise3, LOG_LEVEL_INF);
+LOG_MODULE_REGISTER(Lesson3_Exercise1, LOG_LEVEL_INF);
 
 #define COMPANY_ID_CODE            0x0059
 #define DEVICE_NAME     CONFIG_BT_DEVICE_NAME
@@ -16,6 +17,7 @@ LOG_MODULE_REGISTER(Lesson2_Exercise3, LOG_LEVEL_INF);
 #define USER_BUTTON DK_BTN1_MSK
 #define RUN_STATUS_LED DK_LED1
 #define RUN_LED_BLINK_INTERVAL 1000
+#define CONNECTION_STATUS_LED   DK_LED2
 
 static const struct bt_le_adv_param *adv_param =
 	BT_LE_ADV_PARAM(BT_LE_ADV_OPT_CONNECTABLE|BT_LE_ADV_OPT_USE_IDENTITY,
@@ -32,8 +34,31 @@ static const struct bt_data sd[]={                              /*scan response 
     BT_DATA_BYTES(BT_DATA_UUID128_ALL, BT_UUID_128_ENCODE(0x00001523, 0x1212, 0xefde, 0x1523, 0x785feabcd123)),
 };
 
+void on_connected(struct bt_conn *conn, uint8_t err)
+{
+    if (err) {
+        LOG_ERR("Connection error %d", err);
+        return;
+    }
+    LOG_INF("Connected");
+  //  my_conn = bt_conn_ref(conn);
+    dk_set_led(CONNECTION_STATUS_LED, 1);
+
+}
+
+void on_disconnected(struct bt_conn *conn, uint8_t reason)
+{
+    LOG_INF("Disconnected. Reason %d", reason);
+   // bt_conn_unref(my_conn);
+    dk_set_led(CONNECTION_STATUS_LED, 1);
+   
+}
 int main(void)
 {
+    struct bt_conn_cb connection_callbacks = {
+        .connected              = on_connected,
+        .disconnected           = on_disconnected,
+    };
     int blink_status=0;
     int err;
     bt_addr_le_t addr;
@@ -60,6 +85,8 @@ int main(void)
         return -1;
     }
     LOG_INF(" Bluetooth init");
+
+    bt_conn_cb_register(&connection_callbacks);
 
     err = bt_le_adv_start(adv_param, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
     if (err) {
